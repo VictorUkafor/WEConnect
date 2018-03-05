e59424ad0dc169685d223e97a41941fb7c3a0d96
Victor Ukafor <victorukafor@gmail.com>
2018-03-05 21:08:33 +0100

    writes the POST /auth/signup API to register a user

---
+++ server/lib/db/database.js
@@ -0,0 +1,26 @@
+const database = {
+  users: [
+    {
+      id: 1,
+      firstName: 'Victor',
+      lastName: 'Ukafor',
+      email: 'victorukafor@gmail.com',
+      password: "password123",
+    }, {
+      id: 2,
+      firstName: 'Kent',
+      lastName: 'Westwood',
+      email: 'kentwestwood@gmail.com',
+      password: "password",
+    }
+  ],
+
+  businesses: [],
+  reviews: [],
+
+
+};
+
+
+export default database;
+
