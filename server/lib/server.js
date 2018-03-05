5dc14b30b6b6c278d05dc4a3639a9fff5909859b
Victor Ukafor <victorukafor@gmail.com>
2018-03-05 13:02:03 +0100

    creates file structure and server file in server subfolder

---
+++ server/lib/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => { res.send({ message: 'Welcome to the WEConnect app!' }); });
+
+  app.listen(5000, () => {
+      console.log('listening on port 5000:. . .');
+  });
+
+export default app;
