2a0d3c6318cd5b0f9403fb10c6b07c07515b0463
Victor Ukafor <victorukafor@gmail.com>
2018-03-06 12:54:39 +0100

    adds uuid's id to dummy database

--- server/lib/db/database.js
+++ server/lib/db/database.js
@@ -1,17 +1,19 @@
-const database = {
+import uuid from 'uuid';
+
+export default database = {
   users: [
     {
-      id: 1,
+      id: uuid.v4(),
       firstName: 'Victor',
       lastName: 'Ukafor',
       email: 'victorukafor@gmail.com',
-      password: "password123",
+      password: 'password123',
     }, {
-      id: 2,
+      id: uuid.v4(),
       firstName: 'Kent',
       lastName: 'Westwood',
       email: 'kentwestwood@gmail.com',
-      password: "password",
+      password: 'password',
     }
   ],
 
@@ -20,7 +22,4 @@ const database = {
 
 
 };
-
-
-export default database;
 
