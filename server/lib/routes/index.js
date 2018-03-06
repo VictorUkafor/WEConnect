9b7d1df2c89995fa8fc57537497bed845061d543
Victor Ukafor <victorukafor@gmail.com>
2018-03-06 12:56:09 +0100

    refactors routes file

--- server/lib/routes/index.js
+++ server/lib/routes/index.js
@@ -1,10 +1,9 @@
-import UsersController from '../controllers/usersController';
-
-import database from '../db/database';
-
-const usersController = new UsersController(database);
+import express from 'express';
+const apiRouter = express.Router(); 
+import UsersController from '../controllers/users';
+const usersController = new UsersController(apiRouter);
 
 export default (app) => {
   app.get('/api/v1', (req, res) => res.status(200).send({ message: 'Welcome to the WEConnect routes!' }));
-  app.post('/api/v1/auth/signup', usersController.postUsers);
+  app.use('/api/v1', apiRouter);
 };
