ab58fdee2419c2b9da60a52bd04cc9e147a7fe9f
Victor Ukafor <victorukafor@gmail.com>
2018-03-12 12:05:07 +0100

    restructures files and folders

--- server/lib/controllers/users.js
+++ server/lib/controllers/users.js
@@ -1,17 +1,18 @@
 import uuid from 'uuid';
 import database from '../db/database';
 import UsersService from '../services/users';
+
 const usersService = new UsersService(database);
 
 
 export default class UsersController {
   constructor(router) {
-     this.router = router;
-     this.registerRoutes(); 
+    this.router = router;
+    this.registerRoutes();
   }
 
-   registerRoutes() {
-    this.router.post('/auth/signup', this.postUser.bind(this)); 
+  registerRoutes() {
+    this.router.post('/auth/signup', this.postUser.bind(this));
   }
 
   postUser(req, res) {
@@ -26,13 +27,12 @@ export default class UsersController {
       password: 'The password field is required',
       confirm_password: 'The confirm_password field is required'
     };
-    
+
     if (!Object.keys(userInfo).length > 0) {
       res.status(500).send({
         message: 'All fields are required!'
       });
     } else {
-
       Object.keys(userFields).forEach((field) => {
         if (!userInfo[field]) { errors.push(userFields[field]); }
       });
@@ -41,24 +41,24 @@ export default class UsersController {
         res.status(500).send({ message: errors });
       } else if (userInfo.password !== userInfo.confirm_password) {
        	res.status(500).send({ message: 'Your password did not match!' });
-      } else if (reqEmail){
-          res.status(500).send({ message: 'The user with this email has already been registered!' });
-        } else {
-          const id = uuid.v4();
-          const firstName = userInfo.firstName;
-          const lastName = userInfo.lastName;
-          const email = userInfo.email;
-          const password = userInfo.password;
-          const user = { id, firstName, lastName, email, password, };
+      } else if (reqEmail) {
+        res.status(500).send({ message: 'The user with this email has already been registered!' });
+      } else {
+        const id = uuid.v4();
+        const firstName = userInfo.firstName;
+        const lastName = userInfo.lastName;
+        const email = userInfo.email;
+        const password = userInfo.password;
+        const user = {
+          id, firstName, lastName, email, password,
+        };
 
-          usersService.addUser(user);
-          return res.status(201).send({
-            message: ['A new user has been added successfully',  users.reverse()]
-          });
-       		
+        usersService.addUser(user);
+        return res.status(201).send({
+          message: ['A new user has been added successfully', users.reverse()]
+        });
+      }
     }
   }
-}
-
 }
 
