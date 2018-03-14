81e104fa183043fd9e6cce59d2ee287ef872ce9f
Victor Ukafor <victorukafor@gmail.com>
2018-03-14 04:47:01 +0100

    write tests for "POST /<userId>/businesess/<businessId>/reviews" API

--- test/index.js
+++ test/index.js
@@ -438,5 +438,53 @@ describe('WEConnect API Routes', () => {
     });
   });
 
+    // Testing for 'POST /api/v1/<userId>/businesses/<businessId>/reviews'
+  describe('POST /api/v1/<userId>/businesses/<businessId>/reviews', () => {
+    // Only authenticated users can review a business
+    it('Only authenticated users can review a business', (done) => {
+      request.post('/api/v1/3/businesses/1/reviews')
+        .send({
+          reviewBody: 'Cool business',
+        })
+        .expect(500)
+        .end((err) => {
+          done(err);
+        });
+    });
+
+    // Business can not be found
+    it('Business can not be found', (done) => {
+      request.post('/api/v1/1/businesses/1/reviews')
+        .send({
+          reviewBody: 'Cool business',
+        })
+        .expect(404)
+        .end((err) => {
+          done(err);
+        });
+    });
+
+    // Review content must be entered
+    it('Review content must be entered', (done) => {
+      request.post('/api/v1/1/businesses/2/reviews')
+        .send({})
+        .expect(500)
+        .end((err) => {
+          done(err);
+        });
+    });
+
+    // Review added to a business
+    it('Review added to a business', (done) => {
+      request.post('/api/v1/1/businesses/2/reviews')
+        .send({ reviewBody: 'Cool business', })
+        .expect(201)
+        .end((err) => {
+          done(err);
+        });
+    });
+  });
+
+
   
 });
