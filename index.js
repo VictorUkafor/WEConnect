3efe606cbb1ece8e3b20bbfb2b69cff5fa572c9f
Victor Ukafor <victorukafor@gmail.com>
2018-03-15 08:54:09 +0200

    save files after branching

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import models from 'models';
+import db from 'server/db';
+import app from 'server/boot';
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
