f08cf8d1a25bd21012496c1a5dbc68c8925ee0d8
Victor Ukafor <victorukafor@gmail.com>
2018-03-17 22:19:22 +0200

    install dependencies and adds CI files to root folder

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+import router from './server/routes/index';
+
+const app = express();
+const port = 7000;
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+router(app);
+app.get('/', (req, res) => {});
+
+if(!module.parent){
+	app.listen(port);
+}
+
+export default app;
