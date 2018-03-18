b948985dd17ad49e5e089be34a92afe04c5cdb4a
Victor Ukafor <victorukafor@gmail.com>
2018-03-18 09:25:05 +0200

    adds logic for "POST /auth/login" API endpoint implementation

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -25,6 +25,7 @@ export default class UsersController {
   */
   registerRoutes() {
     this.router.post('/auth/signup', this.postUser.bind(this));
+    this.router.post('/auth/login', this.loginUser.bind(this));
   }
 
   /** An API for adding a new user:
@@ -38,7 +39,7 @@ export default class UsersController {
   postUser(req, res) {
     const { body: userInfo } = req;
     const errors = [];
-    const reqEmail = this.users.find(u => u.email === userInfo.email);
+    const regUser = this.users.find(u => u.email === userInfo.email);
 
     const userFields = {
       firstName: 'The First Name field is required',
@@ -61,7 +62,7 @@ export default class UsersController {
         res.status(500).send({ message: errors });
       } else if (userInfo.password !== userInfo.confirm_password) {
         res.status(500).send({ message: 'Your password did not match!' });
-      } else if (reqEmail) {
+      } else if (regUser) {
         res.status(500).send({ message: 'The user with this email has already been registered!' });
       } else {
         const id = uuid.v4();
@@ -75,9 +76,31 @@ export default class UsersController {
 
         this.users.push(user);
         return res.status(201).send({
-          message: ['A new user has been added successfully', this.users]
+          message: ['A new user has been added successfully', this.users.reverse()]
         });
       }
+    }
+  }
+
+  /**
+   *  An API for logging into the app
+   *  POST: /auth/login
+   *  Takes 2 parameters
+   *  @param {object} req the first parameter
+   *  @param  {object} res the second parameter
+   *
+   *  @returns {object} return an object
+   */
+  loginUser(req, res) {
+    const { body: userInfo } = req;
+    const validate = this.users.find(user => user.email === userInfo.email);
+
+    if (Object.keys(userInfo).length !== 2) {
+      res.status(500).send({ message: 'All fields are required!' });
+    } else if (validate && validate.password === userInfo.password) {
+      res.status(201).send({ message: `Welcome! ${validate.firstName} ${validate.lastName}` });
+    } else {
+      res.status(404).send({ message: 'Invalid email or password!' });
     }
   }
 
