03d86a4aaf3aa3e54462fccf397e42e95a9c07c4
Victor Ukafor <victorukafor@gmail.com>
2018-03-18 08:35:14 +0200

    adds logic for "post /auth/signup" API endpoint implementation

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,10 @@
+import express from 'express';
+import UsersController from '../controllers/users';
+
+const apiRouter = express.Router();
+const usersController = new UsersController(apiRouter);
 
 export default (app) => {
   app.get('/api/v1', (req, res) => res.status(200).send({ message: 'Welcome to the WEConnect app!' }));
+  app.use('/api/v1', apiRouter);
 };
